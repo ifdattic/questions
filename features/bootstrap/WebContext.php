e632107ff52e404be2aa1e1e2c5e577f67e523b7
Andrew Marcinkevicius <andrew.web@ifdattic.com>
2015-03-26 15:18:00 +0200

    Add MinkExtension for web features

---
+++ features/bootstrap/WebContext.php
@@ -0,0 +1,26 @@
+<?php
+
+use Behat\Behat\Context\SnippetAcceptingContext;
+use Behat\Behat\Tester\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode;
+use Behat\Gherkin\Node\TableNode;
+use Behat\MinkExtension\Context\MinkContext;
+use Doctrine\ORM\EntityManager;
+
+class WebContext extends MinkContext implements SnippetAcceptingContext
+{
+    /** @var EntityManager */
+    private $em;
+
+    /**
+     * Initializes context.
+     *
+     * Every scenario gets its own context instance.
+     * You can also pass arbitrary arguments to the
+     * context constructor through behat.yml.
+     */
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+}
